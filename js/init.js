393060b2434ba8ac3c11af8a2332f4b209e78d79
left4mad <edleft4mad@gmail.com>
2019-01-19 18:09:15 +0200

    main page fix + product-slider

--- js/init.js
+++ js/init.js
@@ -10,3 +10,17 @@ $('.slider-right').click(function() {
 $('.slider-left').click(function() {
    $('#header-carousel').trigger('prev.owl.carousel');
 });
+
+$('.product-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    dots:false,
+    nav: false,
+    items: 3
+})
+$('.slider-right').click(function() {
+   $('.product-carousel').trigger('next.owl.carousel');
+});
+$('.slider-left').click(function() {
+   $('.product-carousel').trigger('prev.owl.carousel');
+});
